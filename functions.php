dd4c74614e87cd530de86741ee5dc7157049fd81
people code editor <ashleycoolperson101@gmail.com>
2025-08-29 17:47:10 +0100

    Delete functions.php

--- functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-function people_code_editor_enqueue_styles() {
-    wp_enqueue_style('people-style', get_stylesheet_uri());
-}
-add_action('wp_enqueue_scripts', 'people_code_editor_enqueue_styles');
